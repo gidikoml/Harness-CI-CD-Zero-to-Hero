1f2732b46aed1bc4b8f1f95760e985936927b710
Komlavi Gidi <aroyovo376@gmail.com>
2026-07-04 23:00:58 -0400

    DevOps project: Flask Docker Kubernetes

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "application": "Harness DevOps Project",
+        "status": "Running",
+        "version": "1.0.0"
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "status": "UP"
+    }
+
+@app.route("/version")
+def version():
+    return {
+        "version": "1.0.0"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
